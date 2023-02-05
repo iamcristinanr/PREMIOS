35bfde2ec6d254c8740c915f49550e730e57eaa1
Cristina <crisnieromero@gmail.com>
2023-02-05 19:20:30 +0100

    Corregidos typos y creando tests

--- premios/polls/models.py
+++ premios/polls/models.py
@@ -10,8 +10,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    def was_pubished_recently(self):
-        return self.pub_date >= timezone.now() - datatime.timedelta(days=1)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
